a2d17650a182c84ee5d0250ae163c5c78da4ac17
Paulo <paulorabelonetoo@gmail.com>
2025-06-23 13:10:50 -0300

    function operation mark1

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ const chalk = require('chalk');
 
 const fs = require('fs');
 
-console.log(chalk.green("iniciamos o accounts guys!"));
+operation();
+
+function operation(){
+    inquire.prompt([{
+        type:'list',
+        name:'action',
+        message:'What do you want to do?',
+        choices:[
+            'Create Account',
+            'Check Balance',
+            'Deposit',
+            'Withdraw',
+            'Logout'
+        ]
+    }])
+    .then((anwser) => {
+        const action = anwser['action'];
+
+        console.log(chalk.bgBlue.black.bold(`You chose: ${action}`))
+    })
+    .catch((err) => console.log(err));
+}
